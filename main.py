d331d112c624506e3ac15b7fbd019dddd6538e81
aayanb09 <behaayan@gmail.com>
2025-09-23 21:47:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
+app = Flask(__name__)
+app.secret_key = "password"  
+
 @app.route("/")
 def home():
     return render_template("landing.html")
