b0e78c74feeb35d8ce548a63354821fd918807ea
aayanb09 <behaayan@gmail.com>
2025-10-03 20:06:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import os
 
 app = Flask(__name__)
 app.secret_key = "password"  
@@ -6,6 +7,8 @@ app.secret_key = "password"
 @app.route("/")
 def home():
     return render_template("landing.html")
+    test_value = os.getenv("TEST_KEY", "Not set")
+    return render_template("landing.html", test_value=test_value)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
