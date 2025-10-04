c80b6c3b1410589fc573f905394219b1aaa16e64
aayanb09 <behaayan@gmail.com>
2025-10-03 20:08:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,17 @@ def home():
     test_value = os.getenv("TEST_KEY", "Not set")
     return render_template("landing.html", test_value=test_value)
 
+@app.route("/reminders")
+def reminder():
+    return render_template("reminders.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/symptomTracker")
+def symptom():
+    return render_template("symptomReport.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
